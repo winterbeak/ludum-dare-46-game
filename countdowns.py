f6f344da21537bd68b16adbfbc93a9adcc034020
winterbeak <winterbeak@gmail.com>
2020-07-05 16:40:35 -0400

    Refactor: Move countdown graphics to new "countdowns.py" file

---
+++ countdowns.py
@@ -0,0 +1,91 @@
+import pygame
+import random
+
+import graphics
+import colors
+
+
+# Countdown stuff
+def draw_debug(end_time, surface, position):
+    """ Draws a primitive, unstylized countdown. """
+    time = (end_time - pygame.time.get_ticks()) / 1000
+    text = graphics.tahoma.render(str(time), False, colors.WHITE)
+    surface.blit(text, position)
+
+
+numbers = graphics.SpriteColumn("images/numbers.png", 11)
+numbers_small = graphics.SpriteColumn("images/numbers_small.png", 11)
+
+
+def render_number(text, color, shake=0):
+    """ Renders a number using numbers.png.
+
+    Supports the : character.  Ignores the negative sign.
+    """
+    width = numbers.single_width * len(text)
+    surface = graphics.new_surface((width, numbers.single_height))
+
+    x = 0
+    for char in text:
+        if char == "-":
+            continue
+
+        if char == ":":
+            sprite_num = 10
+        else:
+            sprite_num = int(char)
+
+        x_offset = round((random.random() - 0.5) * (shake * 2))
+        y_offset = round((random.random() - 0.5) * (shake * 2))
+
+        numbers.draw(surface, (x + x_offset, y_offset), sprite_num)
+        x += numbers.single_width
+
+    pixel_array = pygame.PixelArray(surface)
+    pixel_array.replace(colors.TEXT_PLACEHOLDER, color)
+
+    return surface
+
+
+def render_small_number(text, color, shake=0):
+    """ Renders a number using numbers_small.png.
+
+    Supports the . character.
+    """
+    width = numbers_small.single_width * len(text)
+    surface = graphics.new_surface((width, numbers_small.single_height))
+
+    x = 0
+    for char in text:
+        if char == ".":
+            sprite_num = 10
+        else:
+            sprite_num = int(char)
+
+        x_offset = round((random.random() - 0.5) * (shake * 2))
+        y_offset = round((random.random() - 0.5) * (shake * 2))
+
+        numbers_small.draw(surface, (x + x_offset, y_offset), sprite_num)
+        x += numbers_small.single_width
+
+    pixel_array = pygame.PixelArray(surface)
+    pixel_array.replace(colors.TEXT_PLACEHOLDER, color)
+
+    return surface
+
+
+def draw(surface, color, time, position, shake=0):
+    """ Draws the countdown to a given time.
+
+    The time variable is a 3-tuple in minutes/seconds/milliseconds form.
+    """
+    minutes, seconds, milliseconds = time
+
+    number = render_number("%d:%02d" % (minutes, seconds), color, shake)
+    small_number = render_small_number(".%03d" % milliseconds, color, shake)
+
+    surface.blit(number, position)
+
+    x = position[0] + number.get_width()
+    y = position[1] + number.get_height() - small_number.get_height()
+    surface.blit(small_number, (x, y))
--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 import sound
 
 import pygame
-import random
 
 import events
 import window
@@ -15,6 +14,7 @@ import colors
 
 import bottles
 import incidents
+import countdowns
 
 # TODO: Fix this window sizing code.
 # Sets window size
@@ -114,92 +114,6 @@ win_sound = sound.load_numbers("win%d", 1)
 win_sound.set_volumes(0.5)
 lose_sound = sound.load_numbers("lose%d", 1)
 lose_sound.set_volumes(0.4)
-
-
-# Countdown stuff
-def draw_debug_countdown(end_time, surface, position):
-    """ Draws a primitive, unstylized countdown. """
-    time = (end_time - pygame.time.get_ticks()) / 1000
-    text = graphics.tahoma.render(str(time), False, colors.WHITE)
-    surface.blit(text, position)
-
-
-numbers = graphics.SpriteColumn("images/numbers.png", 11)
-numbers_small = graphics.SpriteColumn("images/numbers_small.png", 11)
-
-
-def render_number(text, color, shake=0):
-    """ Renders a number using numbers.png.
-
-    Supports the : character.  Ignores the negative sign.
-    """
-    width = numbers.single_width * len(text)
-    surface = graphics.new_surface((width, numbers.single_height))
-
-    x = 0
-    for char in text:
-        if char == "-":
-            continue
-
-        if char == ":":
-            sprite_num = 10
-        else:
-            sprite_num = int(char)
-
-        x_offset = round((random.random() - 0.5) * (shake * 2))
-        y_offset = round((random.random() - 0.5) * (shake * 2))
-
-        numbers.draw(surface, (x + x_offset, y_offset), sprite_num)
-        x += numbers.single_width
-
-    pixel_array = pygame.PixelArray(surface)
-    pixel_array.replace(colors.TEXT_PLACEHOLDER, color)
-
-    return surface
-
-
-def render_small_number(text, color, shake=0):
-    """ Renders a number using numbers_small.png.
-
-    Supports the . character.
-    """
-    width = numbers_small.single_width * len(text)
-    surface = graphics.new_surface((width, numbers_small.single_height))
-
-    x = 0
-    for char in text:
-        if char == ".":
-            sprite_num = 10
-        else:
-            sprite_num = int(char)
-
-        x_offset = round((random.random() - 0.5) * (shake * 2))
-        y_offset = round((random.random() - 0.5) * (shake * 2))
-
-        numbers_small.draw(surface, (x + x_offset, y_offset), sprite_num)
-        x += numbers_small.single_width
-
-    pixel_array = pygame.PixelArray(surface)
-    pixel_array.replace(colors.TEXT_PLACEHOLDER, color)
-
-    return surface
-
-
-def draw_countdown(surface, color, time, position, shake=0):
-    """ Draws the countdown to a given time.
-
-    The time variable is a 3-tuple in minutes/seconds/milliseconds form.
-    """
-    minutes, seconds, milliseconds = time
-
-    number = render_number("%d:%02d" % (minutes, seconds), color, shake)
-    small_number = render_small_number(".%03d" % milliseconds, color, shake)
-
-    surface.blit(number, position)
-
-    x = position[0] + number.get_width()
-    y = position[1] + number.get_height() - small_number.get_height()
-    surface.blit(small_number, (x, y))
 
 
 class PlayScreen:
@@ -568,7 +482,7 @@ class PlayScreen:
         else:
             time = time_math.min_sec_ms_time_to(self.ambulance_time)
         position = self.AMBULANCE_TIMER_POSITION
-        draw_countdown(surface, colors.AMBULANCE_RED, time, position)
+        countdowns.draw(surface, colors.AMBULANCE_RED, time, position)
 
         # Homunculus timer
         milliseconds = time_math.ms_time_to(self.death_time)
@@ -588,7 +502,7 @@ class PlayScreen:
         else:
             time = time_math.min_sec_ms_time_to(self.death_time)
         position = self.HOMUNCULUS_TIMER_POSITION
-        draw_countdown(surface, color, time, position, shake)
+        countdowns.draw(surface, color, time, position, shake)
 
     def draw_controls(self, surface):
         # Feed text
