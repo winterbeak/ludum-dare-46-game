d045bb8422550db4182eb547b2f0e79ce2035780
winterbeak <winterbeak@gmail.com>
2020-09-06 13:18:26 -0400

    Feature: Add Point class
    This class makes it easier to deal with coordinates, referencing them
    by variable.x, variable.y, rather than the old variable[0], variable[1].

--- geometry.py
+++ geometry.py
@@ -1,3 +1,21 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def coords(self):
+        return self.x, self.y
+
+    @coords.setter
+    def coords(self, value):
+        self.x = value[0]
+        self.y = value[1]
+
+    def offset(self, x, y):
+        return self.x + x, self.y + y
+
+
 def centered(container,  object_size):
     container_x, container_y, container_w, container_h = container
     object_width, object_height = object_size
